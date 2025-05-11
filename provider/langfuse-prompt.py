4868d33cac621b1b5148b55dac67269014434fcf
Thomas Drebes <38798057+tdrebes@users.noreply.github.com>
2025-05-12 01:21:49 +0200

    Implement basic prompts

--- provider/langfuse-prompt.py
+++ provider/langfuse-prompt.py
@@ -5,13 +5,14 @@ from dify_plugin.errors.tool import ToolProviderCredentialValidationError
 
 from langfuse import Langfuse
 
+
 class LangfusePromptProvider(ToolProvider):
     def _validate_credentials(self, credentials: dict[str, Any]) -> None:
         try:
             host = credentials.get("langfuse_host")
             secret_key = credentials.get("langfuse_secret_key")
             public_key = credentials.get("langfuse_public_key")
-        
+
             lf = Langfuse(host=host, secret_key=secret_key, public_key=public_key)
             lf.auth_check()
         except Exception as e:
--- tools/langfuse-prompt.py
+++ tools/langfuse-prompt.py
@@ -4,8 +4,25 @@ from typing import Any
 from dify_plugin import Tool
 from dify_plugin.entities.tool import ToolInvokeMessage
 
+from langfuse import Langfuse
+
+
 class LangfusePromptTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
-        yield self.create_json_message({
-            "result": "Hello, world!"
-        })
+        lf = Langfuse(
+            host=self.runtime.credentials["langfuse_host"],
+            secret_key=self.runtime.credentials["langfuse_secret_key"],
+            public_key=self.runtime.credentials["langfuse_public_key"],
+        )
+
+        prompt = lf.get_prompt(
+            name=tool_parameters["prompt_name"],
+            version=tool_parameters["prompt_version"],
+            label=tool_parameters["prompt_label"],
+        )
+        if prompt is None:
+            raise ValueError(f"Prompt {tool_parameters['prompt_name']} not found")
+
+        # TODO: cache & support compiled prompts (maybe separate tool) & langfuse observability
+
+        yield self.create_text_message(prompt.prompt)
