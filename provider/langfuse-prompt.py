fd3a620fba7565f4223b417f221c48361a8ed0a2
Thomas Drebes <38798057+tdrebes@users.noreply.github.com>
2025-05-12 00:54:01 +0200

    Authenticate and validate credentials

--- provider/langfuse-prompt.py
+++ provider/langfuse-prompt.py
@@ -3,12 +3,16 @@ from typing import Any
 from dify_plugin import ToolProvider
 from dify_plugin.errors.tool import ToolProviderCredentialValidationError
 
+from langfuse import Langfuse
 
 class LangfusePromptProvider(ToolProvider):
     def _validate_credentials(self, credentials: dict[str, Any]) -> None:
         try:
             host = credentials.get("langfuse_host")
             secret_key = credentials.get("langfuse_secret_key")
-            
+            public_key = credentials.get("langfuse_public_key")
+        
+            lf = Langfuse(host=host, secret_key=secret_key, public_key=public_key)
+            lf.auth_check()
         except Exception as e:
             raise ToolProviderCredentialValidationError(str(e))
